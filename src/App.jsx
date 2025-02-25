29f723bb0fddb1f180011558a30caaf5c8aaf17b
fitse01 <fitsumtafese01@gmail.com>
2025-02-25 07:30:49 +0300

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import NavBar from "@/components/common/NavBar";
+import Footer from "@/components/common/Footer";
+import Home from "@/pages/home";
+import Shop from "@/pages/shop";
+import Contact from "@/pages/contact";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="flex max-sm:flex-col max-sm:bg-primary-100 gap-20  items-center justify-center">
-      <p className="text-3xl w-[300px] text-primary bg-primary-100 flex justify-center items-center my-20 font-italianno">
-        This is from react , tailwind v-4 and shadCn setup
-      </p>
-      <a href="/login">
-        <Button variant="default" className="bg-blue-300 text-black">
-          Login
-        </Button>
-      </a>
-
-      <div className="hidden  max-sm:flex">
-        <Sheet className="bg-white text-black">
-          <SheetTrigger>Open</SheetTrigger>
-          <SheetContent>
-            <div className="flex flex-col items-center justify-center gap-10 my-20 text-black font-montaga ">
-              <a href="/" className="hover:text-blue-800">
-                Home
-              </a>
-              <a href="/shop" className="hover:text-gray-600">
-                {" "}
-                Shope
-              </a>
-              <a href="/new-arrivals" className="hover:text-gray-600">
-                New Arrivals
-              </a>
-              <a href="/sales-ofers" className="hover:text-gray-600">
-                Sales & Offers{" "}
-              </a>
-              <a href="/contact" className="hover:text-gray-600">
-                Contact
-              </a>
-            </div>
-          </SheetContent>
-        </Sheet>
-      </div>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
-
 export default App;
---
+++ src/components/common/NavBar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const NavBar = () => {
+  return <div>NavBar</div>;
+};
+
+export default NavBar;
+// import { Button } from "@/components/ui/button";
+// import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+// const NavBar = () => {
+//   return (
+//     <div className="flex max-sm:flex-col max-sm:bg-primary-100 gap-20  items-center justify-center">
+//       <p className="text-3xl w-[300px] text-primary bg-primary-100 flex justify-center items-center my-20 font-italianno">
+//         This is from react , tailwind v-4 and shadCn setup
+//       </p>
+//       <a href="/login">
+//         <Button variant="default" className="bg-blue-300 text-black">
+//           Login
+//         </Button>
+//       </a>
+
+//       <div className="hidden  max-sm:flex">
+//         <Sheet className="bg-white text-black">
+//           <SheetTrigger>Open</SheetTrigger>
+//           <SheetContent>
+//             <div className="flex flex-col items-center justify-center gap-10 my-20 text-black font-montaga ">
+//               <a href="/" className="hover:text-blue-800">
+//                 Home
+//               </a>
+//               <a href="/shop" className="hover:text-gray-600">
+//                 {" "}
+//                 Shope
+//               </a>
+//               <a href="/new-arrivals" className="hover:text-gray-600">
+//                 New Arrivals
+//               </a>
+//               <a href="/sales-ofers" className="hover:text-gray-600">
+//                 Sales & Offers{" "}
+//               </a>
+//               <a href="/contact" className="hover:text-gray-600">
+//                 Contact
+//               </a>
+//             </div>
+//           </SheetContent>
+//         </Sheet>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NavBar;
